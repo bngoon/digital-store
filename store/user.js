c49233bc236bcf6f81bed08775928022f8c6b313
Booker Ngoon <bookerngoon@gmail.com>
2024-04-18 10:38:36 -0400

    first commit, store, user.js set up, git initialized

---
+++ store/user.js
@@ -0,0 +1,14 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+let store = (set) => ({
+  isLoggedIn: false,
+  setUserState: (isLoggedIn) => set(() => ({ isLoggedIn })),
+});
+
+// persist the state with key "randomKey"
+store = persist(store, { name: "user-store" });
+//create the store
+let useStore = create(store);
+
+export default useStore;
