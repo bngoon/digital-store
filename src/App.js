4b7030448dd5d6339bb3acf229514042e7461c92
Booker Ngoon <bookerngoon@gmail.com>
2024-04-18 11:52:51 -0400

    navbar built with css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar.jsx";
 
 function App() {
-  return <div className="App">React</div>;
+  return (
+    <div className="app-wrapper">
+      <Navbar />
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
