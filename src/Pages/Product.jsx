5a0891cf8f5444911ca52e51f6f3d9d491c8ee35
Booker Ngoon <bookerngoon@gmail.com>
2024-04-26 17:12:46 -0400

    produc.jsx built

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { ShopContext } from '../Context/ShopContext';
+
 
 function Product() {
+  const {all_product } = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((e)=> e.id ===productId)
   return (
-    <div>Product</div>
+    <div className=''></div>
   )
 }
 
