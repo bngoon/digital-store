ca4592513e0a5c6371e39bd148f417db74842129
Booker Ngoon <bookerngoon@gmail.com>
2024-04-26 17:18:49 -0400

    breadcrum and productsjsx file finsihed

---
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Breadcrum.css';
+import arrow_icon from '../Assets/breadcrum_arrow.png';
+function Breadcrum(props) {
+    const{product} = props;
+  return (
+    <div className='breadcrum'>
+      HOME <img src={arrow_icon} alt="arrow" /> SHOP <img src={arrow_icon} alt="arrow" /> {product.category} <img src={arrow_icon} alt="arrow" /> {product.name}
+    </div>
+  )
+}
+
+export default Breadcrum
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { ShopContext } from '../Context/ShopContext';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
 
 
 function Product() {
   const {all_product } = useContext(ShopContext);
   const {productId} = useParams();
-  const product = all_product.find((e)=> e.id ===productId)
+  const product = all_product.find((e)=> e.id === Number(productId));
   return (
-    <div className=''></div>
+    <div className=''>
+      <Breadcrum product={product} />
+    </div>
   )
 }
 
